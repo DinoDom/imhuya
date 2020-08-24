df17185235ee4adf99ca0fbd4725420db441a2ca
DinoDom <dinotang91@gmail.com>
2020-08-24 23:29:48 +0800

    修改发一条弹幕的快捷键为Ctrl+Alt+W 修改刷礼物方式为递归,每次只刷一个直播间

--- src/meta.js
+++ src/meta.js
@@ -1,13 +1,11 @@
 // ==UserScript==
 // @name        我是一颗小虎牙
 // @namespace   Violentmonkey Scripts
-// @version     0.14
+// @version     0.12
 // @icon        https://www.huya.com/favicon.ico
 // @match       *://www.huya.com/*
-// @match       *://a.msstatic.com/*
 // @author      Dino
 // @require     https://cdn.staticfile.org/jquery/3.3.1/jquery.min.js
-// @require     https://greasyfork.org/scripts/408149-notice-js/code/Noticejs.js?version=833673
 // @description 自动发弹幕、自动领取每日宝箱、自动领取礼物宝箱、自动领取每日任务经验、自动切换最高清晰度、获取直播流地址、一键给有牌子的主播刷虎粮、一键过竞猜任务，简洁的界面，快捷键操作等(Ctrl+Q:切换聊天界面的显示  Ctrl+M:静音  Ctrl+B:是否显示弹幕  在弹幕输入框按上下键可以载入弹幕发送历史)
 // @grant       GM_xmlhttpRequest
 // @grant       GM_setClipboard
